0974529a75f9f1f9479a76c15767cada29c254a3
AntonMangirov <antonmangirov@gmail.com>
2025-06-20 07:20:40 +0300

    init ReactRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./components/App";
 
@@ -8,7 +9,9 @@ const element = document.getElementById("root");
 if (element) {
   createRoot(element).render(
     <StrictMode>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </StrictMode>
   );
 }
