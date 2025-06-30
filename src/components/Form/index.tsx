040f35508f6ffcc07905a7868ad7cf3698c89fa2
AntonMangirov <antonmangirov@gmail.com>
2025-06-30 03:41:07 +0300

    create form

---
+++ src/components/Form/index.tsx
@@ -0,0 +1,10 @@
+import { Header } from "../Header";
+
+export const Form = () => {
+  return (
+    <div>
+      <Header />
+      <main></main>
+    </div>
+  );
+};
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import { Home } from "./components/Home";
+import { Form } from "./components/Form";
 
 export const Routers = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/form" element={<Form />} />
     </Routes>
   );
 };
