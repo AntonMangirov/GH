f78bfa838b2d6672d7d7a67f9f42c34a00caaf66
AntonMangirov <antonmangirov@gmail.com>
2025-07-01 03:30:57 +0300

    inst correct router version

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router";
 import { Home } from "./components/Home";
 import { Form } from "./components/Form";
 
