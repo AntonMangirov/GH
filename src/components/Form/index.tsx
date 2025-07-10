1a3c8a119ac405cd21f3867371222aeb1e4afd1b
AntonMangirov <antonmangirov@gmail.com>
2025-07-10 05:10:05 +0300

    add form expense

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -18,6 +18,51 @@ const Form = () => {
         placeholder="Инвестиции, аренда и т.д."
         variant="outlined"
       />
+      <Typography variant="h6" gutterBottom color="primary">
+        Ежемесячные расходы
+      </Typography>
+
+      <TextField
+        fullWidth
+        label="Жилье"
+        placeholder="Аренда, ипотека, коммунальные"
+        variant="outlined"
+      />
+
+      <TextField
+        fullWidth
+        label="Питание"
+        placeholder="Продукты, рестораны"
+        variant="outlined"
+      />
+
+      <TextField
+        fullWidth
+        label="Транспорт"
+        placeholder="Бензин, общественный транспорт"
+        variant="outlined"
+      />
+
+      <TextField
+        fullWidth
+        label="Здоровье"
+        placeholder="Медицина, фитнес, аптека"
+        variant="outlined"
+      />
+
+      <TextField
+        fullWidth
+        label="Развлечения"
+        placeholder="Кино, хобби, подписки"
+        variant="outlined"
+      />
+
+      <TextField
+        fullWidth
+        label="Другие расходы"
+        placeholder="Одежда, подарки, прочее"
+        variant="outlined"
+      />
     </Container>
   );
 };
