ca06ef2bf06badf29a3aa51384e7d08855cbbdb9
AntonMangirov <antonmangirov@gmail.com>
2025-07-11 02:20:07 +0300

    add useState for income

--- src/components/Form/components/income.tsx
+++ src/components/Form/components/income.tsx
@@ -1,6 +1,10 @@
 import { Paper, Typography, TextField, Grid } from "@mui/material";
+import { useState } from "react";
 
 const Income = () => {
+  const [mainIncome, setMainIncome] = useState("");
+  const [extraIncome, setExtraIncome] = useState("");
+
   return (
     <Paper elevation={5} sx={{ p: 3, mb: 3 }}>
       <Typography variant="h6" color="primary">
@@ -14,6 +18,8 @@ const Income = () => {
             label="Основной доход"
             placeholder="Зарплата, пенсия"
             variant="outlined"
+            value={mainIncome}
+            onChange={(e) => setMainIncome(e.target.value)}
           />
         </Grid>
         <Grid size={6}>
@@ -22,6 +28,8 @@ const Income = () => {
             label="Дополнительный доход"
             placeholder="Инвестиции, аренда и т.д."
             variant="outlined"
+            value={extraIncome}
+            onChange={(e) => setExtraIncome(e.target.value)}
           />
         </Grid>
       </Grid>
